9243c544efb18da50befac07a8f5e0bdb1a8ba51
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-07-05 12:03:35 +0300

    0.0.2

--- T/Basic.php
+++ T/Basic.php
@@ -1,11 +1,25 @@
 <?php
 namespace Dfe\ZohoBooks\T;
 use Dfe\ZohoBooks\Settings as S;
+use Zend_Http_Client as C;
 // 2017-07-05
 final class Basic extends TestCase {
 	/** @test 2017-07-05 */
 	function t00() {}
 
+	/** 2017-07-05 */
+	function t01_token() {echo S::s()->token();}
+
 	/** @test 2017-07-05 */
-	function t01() {echo S::s()->token();}
+	function t02_organizations() {
+		/** @var C $c */
+		$c = new C;
+		$c->setConfig(['timeout' => 120]);
+		$c->setHeaders([
+			'Accept' => 'application/json'
+			,'Authorization' => 'Zoho-authtoken ' . S::s()->token()
+		]);
+		$c->setUri('https://books.zoho.com/api/v3/organizations');
+		echo df_json_encode_pretty(df_ksort_r(df_json_decode($c->request()->getBody())));
+	}
 }
