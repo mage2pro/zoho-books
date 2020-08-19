2c50de737a58269f02133907c8fa335e396c4e26
Dmitry Fedyuk <dfediuk@gmail.com>
2020-08-19 06:50:30 +0600

    https://github.com/mage2pro/core/issues/136

--- Test/Basic.php
+++ Test/Basic.php
@@ -2,7 +2,7 @@
 namespace Dfe\ZohoBooks\Test;
 use Dfe\ZohoBooks\API\Facade as F;
 use Dfe\ZohoBooks\Settings as S;
-// 2017-07-05
+# 2017-07-05
 final class Basic extends TestCase {
 	/** @test 2017-07-05 */
 	function t00() {}
