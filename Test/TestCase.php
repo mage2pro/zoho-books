d5dc556121eff3f0d5efe132ed22ff17703a052a
Dmitrii Fediuk <dfediuk@gmail.com>
2023-02-27 19:54:36 +0300

    0.2.1

--- Test/TestCase.php
+++ Test/TestCase.php
@@ -4,4 +4,4 @@ namespace Dfe\ZohoBooks\Test;
  * 2017-07-05
  * @see \Dfe\ZohoBooks\Test\Basic
  */
-abstract class TestCase extends \Df\ZohoBI\Test\TestCase {}
+abstract class TestCase extends \Dfe\ZohoBI\Test\TestCase {}
